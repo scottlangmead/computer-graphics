03da36a1beeae416e0bdf0099e0a5c4d5c1797ad
Scott <scottlangmead@hotmail.co.uk>
2022-03-15 02:59:38 +0000

    Coursework start
    Simple environment and free cam created.

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -5,60 +5,148 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-geometry geom;
+map<string, mesh> meshes;
 effect eff;
-target_camera cam;
+texture tex;
+free_camera cam;
 
-bool load_content() {
-  // Create triangle data
-  vector<vec3> positions{
-	  vec3(0.0f, 1.0f, 0.0f), vec3(-1.0f, -1.0f, 0.0f), vec3(1.0f, -1.0f, 0.0f)
-  };
-  // Colours
-  vector<vec4> colours{vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f)};
-  // Add to the geometry
-  geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
-  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
+// Cursor position
+double cursor_x = 0.0;
+double cursor_y = 0.0;
+
+bool initialise()
+{
+  // Set input mode - hide the cursor
+  glfwSetInputMode(renderer::get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // Capture initial mouse position
+  glfwGetCursorPos(renderer::get_window(), &cursor_x, &cursor_y);
+  return true;
+}
+
+bool load_content()
+{
+  // Create plane mesh
+  meshes["plane"] = mesh(geometry_builder::create_plane());
+
+  // Create scene
+  meshes["box"] = mesh(geometry_builder::create_box());
+  meshes["tetra"] = mesh(geometry_builder::create_tetrahedron());
+  meshes["pyramid"] = mesh(geometry_builder::create_pyramid());
+  meshes["disk"] = mesh(geometry_builder::create_disk(20));
+  meshes["cylinder"] = mesh(geometry_builder::create_cylinder(20, 20));
+  meshes["sphere"] = mesh(geometry_builder::create_sphere(20, 20));
+  meshes["torus"] = mesh(geometry_builder::create_torus(20, 20, 1.0f, 5.0f));
+
+  // Transform objects
+  meshes["box"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+  meshes["box"].get_transform().translate(vec3(-10.0f, 2.5f, -30.0f));
+  meshes["tetra"].get_transform().scale = vec3(4.0f, 4.0f, 4.0f);
+  meshes["tetra"].get_transform().translate(vec3(-30.0f, 10.0f, -10.0f));
+  meshes["pyramid"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+  meshes["pyramid"].get_transform().translate(vec3(-10.0f, 7.5f, -30.0f));
+  meshes["disk"].get_transform().scale = vec3(3.0f, 1.0f, 3.0f);
+  meshes["disk"].get_transform().translate(vec3(-10.0f, 11.5f, -30.0f));
+  meshes["disk"].get_transform().rotate(vec3(half_pi<float>(), 0.0f, 0.0f));
+  meshes["cylinder"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+  meshes["cylinder"].get_transform().translate(vec3(-25.0f, 2.5f, -25.0f));
+  meshes["sphere"].get_transform().scale = vec3(2.5f, 2.5f, 2.5f);
+  meshes["sphere"].get_transform().translate(vec3(-25.0f, 10.0f, -25.0f));
+  meshes["torus"].get_transform().translate(vec3(-25.0f, 10.0f, -25.0f));
+  meshes["torus"].get_transform().rotate(vec3(half_pi<float>(), 0.0f, 0.0f));
+
+  // Load texture
+  tex = texture("res/textures/check_1.png");
 
   // Load in shaders
-  eff.add_shader("res/shaders/basic.vert", GL_VERTEX_SHADER);
-  eff.add_shader("res/shaders/basic.frag", GL_FRAGMENT_SHADER);
+  eff.add_shader("res/shaders/simple_texture.vert", GL_VERTEX_SHADER);
+  eff.add_shader("res/shaders/simple_texture.frag", GL_FRAGMENT_SHADER);
+
   // Build effect
   eff.build();
 
   // Set camera properties
-  cam.set_position(vec3(0.0f, 0.0f, 10.0f));
+  cam.set_position(vec3(0.0f, 10.0f, 0.0f));
   cam.set_target(vec3(0.0f, 0.0f, 0.0f));
   cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
   return true;
 }
 
-bool update(float delta_time) {
+bool update(float delta_time)
+{
+  // The ratio of pixels to rotation
+  static double ratio_width = quarter_pi<float>() / static_cast<float>(renderer::get_screen_width());
+  static double ratio_height = (quarter_pi<float>() * (static_cast<float>(renderer::get_screen_height()) / static_cast<float>(renderer::get_screen_width()))) / static_cast<float>(renderer::get_screen_height());
+
+  double current_x;
+  double current_y;
+
+  // Get the current cursor position
+  glfwGetCursorPos(renderer::get_window(), &current_x, &current_y);
+
+  // Calculate delta of cursor positions from last frame
+  double delta_x = current_x - cursor_x;
+  double delta_y = current_y - cursor_y;
+
+  // Multiply deltas by ratios - gets actual change in orientation
+  delta_x = delta_x * ratio_width;
+  delta_y = delta_y * ratio_height;
+
+  // Rotate cameras by delta
+  cam.rotate(delta_x, -delta_y);
+
+  // Use keyboard to move the camera - WSAD
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_W))
+	cam.move(vec3(0.0f, 0.0f, 0.1f));
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_S))
+	cam.move(vec3(0.0f, 0.0f, -0.1f));
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_A))
+	cam.move(vec3(-0.1f, 0.0f, 0.0f));
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_D))
+	cam.move(vec3(0.1f, 0.0f, 0.0f));
+
   // Update the camera
   cam.update(delta_time);
+
+  // Update cursor position
+  cursor_x = current_x;
+  cursor_y = current_y;
+
   return true;
 }
 
-bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  // Create MVP matrix
-  mat4 M(1.0f);
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
-  // Render geometry
-  renderer::render(geom);
+bool render()
+{
+  // Render meshes
+  for (auto &e : meshes) {
+	auto m = e.second;
+	// Bind effect
+	renderer::bind(eff);
+	// Create MVP matrix
+	auto M = m.get_transform().get_transform_matrix();
+	auto V = cam.get_view();
+	auto P = cam.get_projection();
+	auto MVP = P * V * M;
+	// Set MVP matrix uniform
+	glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+
+	// Bind and set texture
+	renderer::bind(tex, 0);
+	glUniform1i(eff.get_uniform_location("tex"), 0);
+
+	// Render mesh
+	renderer::render(m);
+  }
+
   return true;
 }
 
-void main() {
+void main()
+{
   // Create application
   app application("Graphics Coursework");
   // Set load content, update and render methods
   application.set_load_content(load_content);
+  application.set_initialise(initialise);
   application.set_update(update);
   application.set_render(render);
   // Run application
